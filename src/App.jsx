2895b983877004c853a9c4c444690fa1dcd70008
mehedi hasan <mhshohan04@gmail.com>
2025-05-04 16:19:37 +0600

    projects setup done with react+tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+    <h2 className='text-rose-600 text-center font-extrabold'>Mehedi Hasan Shohan</h2>
+   </>
+  )
+}
+
+export default App
